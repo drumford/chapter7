f43682951ee301bee54e18981c013efb6c2c9b0a
drumford <don@rumfordproductions.com>
2016-01-03 23:25:24 -0500

    Adding updates...

--- modules/dirlister.py
+++ modules/dirlister.py
@@ -3,6 +3,6 @@ import os
 def run(**args):
     
     files = os.listdir(".")
-    print "[*] In dirlister module.",files
+    print "[*] In dirlister module.",str(files)
     
     return str(files)
